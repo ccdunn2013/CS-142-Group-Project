333807a3bcabd2788fcf2d9d21f76a522078b255
Phaizran <57541255+Phaizran@users.noreply.github.com>
2019-11-21 20:10:52 -0800

    Update Screen.java

--- Screen.java
+++ Screen.java
@@ -2,6 +2,7 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Rectangle;
 
+@SuppressWarnings("unused")
 public abstract class Screen {
 	
 	public abstract void draw(Graphics g, int width, int height);
